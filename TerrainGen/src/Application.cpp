ecec8f1dfaa7718e087626cc3c3f25c1b28db517
Ljamo <masterluka21@gmail.com>
2024-05-20 22:54:40 +1000

    Added primitive shader support.

--- TerrainGen/src/Application.cpp
+++ TerrainGen/src/Application.cpp
@@ -1,10 +1,12 @@
 #include "Application.h"
 
+// Constructor definition
 Application::Application(uint32_t width, uint32_t height, const std::string& title)
     : m_Width(width), m_Height(height), m_Title(title), window(nullptr)
 {
 }
 
+// Framebuffer size callback definition
 void Application::FramebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -40,6 +42,45 @@ bool Application::Init()
 
     glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 
+    glViewport(0, 0, m_Width, m_Height);
+
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+    };
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     return true;
 }
 
@@ -52,6 +93,10 @@ void Application::Run()
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -70,6 +115,10 @@ void Application::processInput(GLFWwindow* window)
 // Cleanup method
 void Application::CleanUp()
 {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
     if (window)
     {
         glfwDestroyWindow(window);
--- TerrainGen/src/Application.h
+++ TerrainGen/src/Application.h
@@ -4,17 +4,33 @@
 class Application
 {
 public:
-	Application(uint32_t width, uint32_t height, const std::string& title);
-	bool Init();
-	void Run();
-	void CleanUp();
+    Application(uint32_t width, uint32_t height, const std::string& title);
+    bool Init();
+    void Run();
+    void CleanUp();
+
 private:
-	static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
-	void processInput(GLFWwindow* window);
+    static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+    void processInput(GLFWwindow* window);
 
-	GLFWwindow* window = nullptr;
+    GLFWwindow* window = nullptr;
+    uint32_t m_Width, m_Height;
+    std::string m_Title;
 
-	uint32_t m_Width, m_Height;
-	std::string m_Title = nullptr;
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+        "}\0";
+
+    GLuint VAO, VBO;
+    GLuint shaderProgram;
 };
-
